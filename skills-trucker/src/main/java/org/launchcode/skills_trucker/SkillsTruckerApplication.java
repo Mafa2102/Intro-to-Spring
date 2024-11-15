f96aa4ad54f372b22975fa75710dacc35e7fa1fb
Maria Usina <maryusina@Marias-Air.attlocal.net>
2024-11-14 21:15:51 -0600

    Initial commit

---
+++ skills-trucker/src/main/java/org/launchcode/skills_trucker/SkillsTruckerApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.skills_trucker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillsTruckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillsTruckerApplication.class, args);
+	}
+
+}
---
+++ skills-trucker/src/test/java/org/launchcode/skills_trucker/SkillsTruckerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.skills_trucker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillsTruckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
